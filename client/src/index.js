90918346a8bdb564b63b5e767afc4f6d10182f66
mccaffertycr <mccaffertycr@gmail.com>
2019-03-03 15:44:57 -0500

    serviceworker

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css';
-import './index.css';
 import App from './App';
 
+import 'normalize.css';
+import './index.css';
+
+import registerServiceWorker from './registerServiceWorker';
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+registerServiceWorker();
